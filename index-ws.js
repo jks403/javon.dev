4416ae6e338f32ad3bb09ef6ae6951ba3ac67419
joshua <joshuaka@joshuakane.net>
2024-03-24 12:02:31 -0700

    adding database to main

--- index-ws.js
+++ index-ws.js
@@ -27,12 +27,21 @@ wss.on('connection', function connection(ws)  {
     if(ws.readyState ===ws.OPEN) {
         ws.send('Welcome to my server');
     }
+
+    db.run(`INSERT INTO visitors(count, time ) 
+        VALUES(${numClients} datetime ('now'))
+        `);
+
     ws.on('close', function close() {
+        wss.broadcast(`current visitors: ${numClients}`);
         console.log('a client has disconnected');
+
+    
     });
 
 
-});
+
+
 
 
 wss.broadcast = function broadcast(data) {
@@ -40,6 +49,53 @@ wss.broadcast = function broadcast(data) {
         client.send(data);
     });
 }
+
+
+
+
+
+process.on('SIGINT', () =>   {
+        server.close(()   => {
+                shutdownDB();
+
+        })
+})
+
+
+/** end websockets */
+
+/** begin database */
+
+const sqlite = require('sqlite3');
+const db = new sqlite.Database(':memory:');
+
+db.serialize(() => {
+        db.run(`
+        CREATE TABLE visitors (
+                count INTEGER, 
+                time TEXT
+        )
+        
+        `)
+
+});
+
+function getCounts() {
+        db.each("SELECT * FROM visitors", (err, row)  =>  {
+                console.log(row);
+        })
+}
+
+
+function shutdownDB()  {
+        getCounts();
+        console.log('Shutting down db');
+        db.close();
+
+}
+
+
+
 
 
 
